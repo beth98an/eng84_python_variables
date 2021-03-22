4ba3a270fd1561e73cc418aa2432f539228e35a0
beth98an <76478762+beth98an@users.noreply.github.com>
2021-03-22 17:04:48 +0000

    updated variables

--- variables.py
+++ variables.py
@@ -1,1 +1,28 @@
-print("hello world")
+# pseudo coding
+
+# Testing the env with hello world message
+# print("hello world")
+#
+# print("Bethan")
+
+# What is a variable?
+# my_name = "Bethan"  # string
+# age = 16  # integer
+# hourly_wage = 8.5  # float
+
+# print(my_name)
+# print(age)
+# print(hourly_wage)
+
+# To get user data we use input()
+first_name = input("Please enter your name ")
+age = input("Pleas enter your age")
+print("Hello, ")
+print(first_name)
+
+# To check the type of data type()
+print(type(age))
+
+#  overwriting a variable
+first_name = "belle"
+print(first_name)
